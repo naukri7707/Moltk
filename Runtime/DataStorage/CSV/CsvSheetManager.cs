c8cd8e25b40114a6bcdcf94685198a4f52f20a97
Naukri <naukri7707@gmail.com>
2024-06-22 13:35:37 +0800

    temp

---
+++ Runtime/DataStorage/CSV/CsvSheetManager.cs
@@ -0,0 +1,38 @@
+﻿using Naukri.InspectorMaid;
+using Naukri.Moltk.DataStorage.Csv;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CsvSheetManager : MonoBehaviour
+{
+    public string csvFilePath;
+
+    public bool useGoogleSheet;
+
+    [ShowIf(nameof(useGoogleSheet))]
+    public string GoogleSheetId;
+
+    private CsvSheet csvSheet;
+
+    public void Save()
+    {
+        csvSheet.Save(csvFilePath);
+    }
+
+    public void Load(CsvSheet csvSheet)
+    {
+        csvSheet.Load(csvFilePath);
+        this.csvSheet = csvSheet;
+    }
+
+    public void UploadToGoogleSheet()
+    {
+        // Todo:
+    }
+
+    public void DownloadFromGoogleSheet()
+    {
+        // Todo:
+    }
+}
